48cf76a06a7206d10cc45fb6075365a2331f2168
arun <arunreddy.iosdev@gmail.com>
2019-06-18 10:21:05 +0530

    Initial Commit

---
+++ Realm Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realm Tutorial
+//
+//  Created by Arun Reddy on 18/06/19.
+//  Copyright © 2019 ITS ME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
